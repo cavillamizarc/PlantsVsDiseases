3e44ffec047c4b5fbfbf451ff0cf7f6d20f25e2e
Cesar Villamizar <cavillamizarc@gmail.com>
2022-12-16 23:32:18 -0500

    Cleaning of DatasetBuilder

--- greeneye/database/DatasetBuilder.py
+++ greeneye/database/DatasetBuilder.py
@@ -5,33 +5,9 @@ from functools import partial
 from typing import Callable, Dict
 
 
-class AbstractDataset(ABC):
-    n_samples: int # type hinting
-    seed: int
-    noise: float
-    raw_f: Callable
-    f: Callable
-
 class DatasetBuilder:
     def __init__():
         ...
         
-    """ def build(self) -> AbstractDataset:
-        match self.dataset_kind:
-            case ClusteringEnum.BLOBS:
-                ds = BlobsDataset(
-                        ClusteringEnum.BLOBS,
-                        **self.params
-                        )
-            case ClusteringEnum.CIRCLES:
-                ds = BiClusterDataset(
-                        ClusteringEnum.CIRCLES,
-                        **self.params
-                        )
-            case ClusteringEnum.MOONS:
-                ds = BiClusterDataset(
-                        ClusteringEnum.MOONS,
-                        **self.params
-                        )
-        ds.init()
-        return ds """
+    def build(self) -> DatasetBuilder:
+        ...
